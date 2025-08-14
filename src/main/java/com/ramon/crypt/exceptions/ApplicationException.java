8f41c01bc5008cd7b8a655b9ee045e90b9aa9c3e
Ramon Saviato <ramonsaviato@hotmail.com>
2025-08-13 22:06:40 -0300

    add ResourceNotFoundException

---
+++ src/main/java/com/ramon/crypt/exceptions/ApplicationException.java
@@ -0,0 +1,43 @@
+package com.ramon.crypt.exceptions;
+
+import org.springframework.http.HttpStatus;
+
+import com.ramon.crypt.util.PathUtils;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class ApplicationException extends RuntimeException {
+    
+     protected final HttpStatus httpStatus;
+    protected final String path;
+
+    public ApplicationException(String message, Throwable cause, HttpStatus httpStatus) {
+        super(message, cause);
+        this.httpStatus = httpStatus;
+        this.path = PathUtils.getCurrentPath();
+    }
+
+    public ApplicationException(String message, Throwable cause) {
+        this(message, cause, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    public ApplicationException(String message, HttpStatus httpStatus) {
+        this(message, null, httpStatus);
+    }
+
+    public ApplicationException(Throwable cause, HttpStatus httpStatus) {
+        this(cause.getMessage(), cause, httpStatus);
+    }
+
+    public ApplicationException(String message) {
+        this(message, (Throwable) null);
+    }
+
+    public ApplicationException() {
+        this("Oops! Something went wrong.");
+    }
+
+}
---
+++ src/main/java/com/ramon/crypt/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,33 @@
+package com.ramon.crypt.services.exceptions;
+
+import org.springframework.http.HttpStatus;
+
+import com.ramon.crypt.exceptions.ApplicationException;
+
+public class ResourceNotFoundException extends ApplicationException {
+
+    public ResourceNotFoundException(String message, Throwable cause, HttpStatus httpStatus) {
+        super(message, cause, httpStatus);
+    }
+
+    public ResourceNotFoundException(String message, Throwable cause) {
+        this(message, cause, HttpStatus.NOT_FOUND);
+    }
+
+    public ResourceNotFoundException(String message, HttpStatus httpStatus) {
+        this(message, null, httpStatus);
+    }
+
+    public ResourceNotFoundException(Throwable cause, HttpStatus httpStatus) {
+        this(cause.getMessage(), cause, httpStatus);
+    }
+
+    public ResourceNotFoundException(String message) {
+        this(message, (Throwable) null);
+    }
+
+    public ResourceNotFoundException() {
+        this("Resource not found.");
+    }
+
+}
